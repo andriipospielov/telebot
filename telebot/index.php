9e99684e8ef49f5c1be2a6d9fea72dc11b9f49aa
andriypospielov <andriypospielov@gmail.com>
2016-11-11 19:59:05 +0200

    First bot implementation

---
+++ telebot/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andriy Pospielov
+ * Date: 11.11.2016
+ * Time: 19:55
+ */
+require_once 'vendor/autoload.php';
+// create a bot
+$bot = new \TelegramBot\Api\Client('272582059:AAH2SX0V7ujCIu5CmY6tFtlNsTXp91j9fpA', '6raD1_umXF24unknJRn647D0vA:I18yW');
+// run, bot, run!
+$bot->run();
+// setting up start command
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Hello world!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
