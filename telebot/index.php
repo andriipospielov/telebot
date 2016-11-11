f6d119bce66b3aefe4c7151597288e22f866359f
andriypospielov <andriypospielov@gmail.com>
2016-11-11 20:14:33 +0200

    added more of some debugging code

--- telebot/index.php
+++ telebot/index.php
@@ -6,7 +6,7 @@
  * Time: 19:55
  */
 require_once 'vendor/autoload.php';
-file_put_contents('log', print_r($_SERVER));
+file_put_contents('log', $_SERVER);
 // create a bot
 $bot = new \TelegramBot\Api\Client('272582059:AAH2SX0V7ujCIu5CmY6tFtlNsTXp91j9fpA', '6raD1_umXF24unknJRn647D0vA:I18yW');
 // run, bot, run!
