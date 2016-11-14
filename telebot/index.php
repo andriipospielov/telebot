4536ea74977b78336619496eb400b0d56a9964b0
andriypospielov <andriypospielov@gmail.com>
2016-11-14 18:41:13 +0200

    implemented primitive bot functionalty

--- telebot/index.php
+++ telebot/index.php
@@ -8,11 +8,38 @@
 require_once 'vendor/autoload.php';
 file_put_contents('log.log', $_SERVER);
 // create a bot
-$bot = new \TelegramBot\Api\Client('272582059:AAH2SX0V7ujCIu5CmY6tFtlNsTXp91j9fpA', '6raD1_umXF24unknJRn647D0vA:I18yW');
+$bot = new \TelegramBot\Api\Client('272582059:AAFGXv_S8XGfvtryd-vCqE2506vBHpPeNi4');
 // run, bot, run!
 $bot->run();
-// setting up start command
+// setting up start commands
 $bot->command('start', function ($message) use ($bot) {
-    $answer = 'Hello world!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
+    $answer = 'Hi there!';
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([['/start', '/enemenemeck']], null, true);
+
+    $bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
 });
+$bot->command('enemenemeck', function ($message) use ($bot) {
+    $answer = 'Do you wanna see some magic?';
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([['/yes', '/no']], null, true);
+
+    $bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
+});
+$bot->command('no', function ($message) use ($bot) {
+    $answer = 'that \'s sad =(';
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([['/start', '/enemenemeck']], null, true);
+
+    $bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
+});
+$bot->command('yes', function ($message) use ($bot) {
+    $answer = 'boom!';
+    $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([['/start', '/enemenemeck']], null, true);
+    $picture = new \CURLFile('image.jpg');
+    $bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
+    $bot->sendPhoto($message->getChat()->getId(),$picture);
+
+});
+
+
+$bot->run();
+
+
